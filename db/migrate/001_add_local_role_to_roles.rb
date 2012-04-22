4ef05147cfcb024d140765d6b31a52b748546c8e
Jan Vlnas <git@jan.vlnas.cz>
2012-04-22 23:58:32 +0200

    Prep migration

---
+++ db/migrate/001_add_local_role_to_roles.rb
@@ -0,0 +1,21 @@
+class AddLocalRoleToRoles < ActiveRecord::Migration
+  def self.up
+    change_table :roles do |t|
+      t.string :type, :limit => 30, :default => "", :null => false
+      t.integer :local_role_project_id
+    end
+    add_index :roles, [:id, :type]
+    add_index :roles, :local_role_project_id
+
+    Role.update_all("type = 'Role'", "type = ''")
+  end
+
+  def self.down
+    LocalRole.destroy_all
+    change_table :roles do |t|
+      t.remove_index :column => [:id, :type]
+      t.remove_index :column => :local_role_project_id
+      t.remove :local_role_project_id, :type # XXX Assumes there's no other plugin using STI
+    end
+  end
+end

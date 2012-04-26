064393425497a406b8d60fb24fdcb341a42cee2c
Jan Vlnas <git@jan.vlnas.cz>
2012-04-26 22:26:33 +0200

    Add report route as collection

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 ActionController::Routing::Routes.draw do |map|
   map.resources :projects do |project|
-    project.resources :local_roles, :except => [:index] #, :member => { :add_users => :post}
+    project.resources :local_roles, :except => [:index] , :collection => { :report => :get}
   end
 
 end

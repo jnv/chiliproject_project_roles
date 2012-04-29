2122cc5001ada1d230162af6a3a9d51f41c3a2aa
Jan Vlnas <git@jan.vlnas.cz>
2012-04-29 20:41:00 +0200

    Role shifts view

--- app/models/role_shift.rb
+++ app/models/role_shift.rb
@@ -2,11 +2,13 @@
 class RoleShift < ActiveRecord::Base
   unloadable
 
+  BUILTINS = [Role::BUILTIN_ANONYMOUS, Role::BUILTIN_NON_MEMBER]
+
   belongs_to :project, :readonly => true
   belongs_to :role#, :readonly => true
 
   validates_presence_of :project, :role
-  validates_inclusion_of :builtin, :in => [Role::BUILTIN_ANONYMOUS, Role::BUILTIN_NON_MEMBER]
+  validates_inclusion_of :builtin, :in => BUILTINS
   validates_uniqueness_of :builtin, :scope => :project_id
 
   attr_readonly :project, :builtin

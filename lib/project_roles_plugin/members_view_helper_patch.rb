30fae687644018cc1c1dd84634aa261a7a97aa75
Jan Vlnas <git@jan.vlnas.cz>
2012-09-11 15:02:49 +0200

    MembersViewHelperPatch: Use super instead of alias_method_chain

--- lib/project_roles_plugin/members_view_helper_patch.rb
+++ lib/project_roles_plugin/members_view_helper_patch.rb
@@ -2,23 +2,11 @@
 module ProjectRolesPlugin
   module MembersViewHelperPatch
 
-    def self.included(base)
-      base.send(:include, InstanceMethods)
-      base.class_eval do
-        unloadable
-        #extend InstanceMethods
-        alias_method_chain :load_roles, :project_roles
-      end
+    def load_roles(project)
+      #XXX could be handled by Project#available_roles but that would break the chain
+      roles = super
+      project.local_roles + roles
     end
-
-    module InstanceMethods
-
-      def load_roles_with_project_roles(project)
-        #XXX could be handled by Project#available_roles but that would break the chain
-        roles = load_roles_without_project_roles(project)
-        project.local_roles + roles
-      end
-    end
-
   end
+
 end

d756d5dc7dfd0e2a00610baf452c9db369c6c93e
Jan Vlnas <git@jan.vlnas.cz>
2012-09-11 15:00:04 +0200

    Bump version

--- init.rb
+++ init.rb
@@ -57,7 +57,7 @@ Redmine::Plugin.register :chiliproject_project_roles do
   name 'Chiliproject Project Roles plugin'
   author 'Jan Vlnas'
   description 'Provides per-project roles and workflows'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/jnv/chiliproject_project_roles'
   author_url 'https://github.com/jnv'
 
